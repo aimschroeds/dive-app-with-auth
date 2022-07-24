1bdd2a4c51a1d2babfe8df3b335e6bbcc954e56d
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2022-07-24 17:38:18 +0200

    Added dummy profile and explore pages

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import AddDiveScreen from './screens/AddDiveScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ExploreScreen from './screens/ExploreScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -18,6 +20,8 @@ export default function App() {
         <Stack.Screen options={{ headerShown:false }} name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Add Dive" component={AddDiveScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Explore" component={ExploreScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
