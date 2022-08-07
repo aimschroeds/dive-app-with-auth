9a96218e13e61bb37f090d6c9b209c6113f402f6
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2022-08-07 13:26:54 +0200

    Updated navigation

--- components/navigationBar.js
+++ components/navigationBar.js
@@ -8,31 +8,42 @@ import navData from '../data/navigation.json';
 export function NavigationBar({ navigation }) {
     const styles = StyleSheet.create({
         navContainer: {
-            absolute: 'absolute',
-            alignItems: 'center',
+            // absolute: 'absolute',
+            // alignItems: 'center',
             marginBottom: 10,
-            paddingLeft: 30,
+            // paddingLeft: 30,
             paddingTop: 100,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            flex: 1,
+
         },
         navBar: {
             flexDirection: 'row',
             width: '100%',
             justifyContent: 'space-evenly',
-            // alignItems: 'center',
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            // alignContent: 'center',
         },
         menuItemText: {
-            fontSize: 18,
+            fontSize: 14,
             fontWeight: "300",
-            color: "grey",
-            // alignSelf: 'center',
+            color: "black",
+            paddingTop: 10,
+            // alignContent: 'center',
 
         },
-        icons: {
+        menuItems: {
             padding: 10,
+            alignItems: 'center',
         }
     });
     let nav_items = navData.navigation;
-    console.log(nav_items);
+
     return (
         
         <View style={styles.navContainer}>
@@ -40,9 +51,9 @@ export function NavigationBar({ navigation }) {
                 {nav_items.map((element) => (
                 <TouchableOpacity 
                     onPress={() => navigation.navigate(element.page)}
-                    style={styles.icons}
+                    style={styles.menuItems}
                 >
-                    <Icon name={element.icon} height='40' width='40' color="grey" />
+                    <Icon name={element.icon} height='30' width='30' color="#413FEB" />
                     <Text style={styles.menuItemText}>{element.title}</Text>
                 </TouchableOpacity>
                 ))}
