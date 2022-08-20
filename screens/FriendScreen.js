925ec885b553eef9e7cb3a7e1c0acd5e20b14fe3
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2022-08-20 17:36:04 +0200

    Managing relationships

--- screens/FriendScreen.js
+++ screens/FriendScreen.js
@@ -17,8 +17,14 @@ const FriendScreen = ( { route, navigation }) => {
   // const [userLastName, setUserLastName] = useState(null)
   const [userProfilePicture, setUserProfilePicture] = useState(null)
   const [userProfilePictureURL, setUserProfilePictureURL] = useState(null)
+  const [relationship, setRelationship] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-  var docRef = db.collection("users").doc(userId);
+  if (userId === auth.currentUser.uid) {
+    navigation.navigate('Profile')
+  }
 
   const goBack = () => {
     navigation.goBack();
@@ -32,15 +38,28 @@ React.useLayoutEffect(() => {
             style={AppStyles.plusButtonText}>Cancel</Text>
         ),
     })
-}, [navigation])
+}, [navigation, relationship])
 
-  docRef.get().then((doc) => {
+if (loading) {
+  var friendRef = db.collection("friends").doc(auth.currentUser.uid);
+  var relationshipRef = friendRef.collection("relationships").doc(userId);
+  relationshipRef.get().then((doc) => {
+    if (doc.exists) {
+        setRelationship(doc.data().status)
+    }
+    else {
+        setRelationship('none')
+    }
+  }).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+  var userRef = db.collection("users").doc(userId);
+  userRef.get().then((doc) => {
       if (doc.exists) {
           setUserDisplayName(doc.data().display_name)
-          // setUserLastName(doc.data().last_name)
           setUserProfilePicture(doc.data().image)
           setImage(userProfilePicture)
-          console.log("Document data:", doc.data());
       } else {
           // doc.data() will be undefined in this case
           console.log("No such document! ", userId);
@@ -48,6 +67,86 @@ React.useLayoutEffect(() => {
   }).catch((error) => {
       console.log("Error getting document:", error);
   });
+
+  setLoading(false);
+}
+  
+
+  const friendStatusUpdate = (action) => {
+    if (action === 'Add' || action === 'Accept') {
+      addFriend(action)
+    }
+    else if (action === 'Decline' || action === 'Remove') {
+      removeFriend()
+    }
+  }
+
+  let removeFriend = async () => {
+    db.collection("friends").doc(auth.currentUser.uid).collection("relationships").doc(userId)
+    .delete()
+    .then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    db.collection("friends").doc(userId).collection("relationships").doc(auth.currentUser.uid)
+    .delete()
+    .then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    setSuccessMessage('Friend removed')
+    setRelationship('none')
+  }
+
+  let addFriend = async (action) => {
+    console.log('action', action)
+    if (action === 'Add') {
+        console.log('add')
+        setRelationship: 'requested'
+        var currentUserStatus = {
+            status: 'requested',
+            createdAt: new Date(),
+            }
+        var secondUserStatus = {
+            status: 'pending',
+            createdAt: new Date(),
+            }
+        }
+    else if (action === 'Accept') {
+        setRelationship: 'accepted'
+        var currentUserStatus = {
+            status: 'friends',
+            createdAt: new Date(),
+            }
+        var secondUserStatus = {
+            status: 'friends',
+            createdAt: new Date(),
+            }
+        }
+    // docRef = 
+    db.collection("friends").doc(auth.currentUser.uid).collection("relationships").doc(userId).set(currentUserStatus)
+    .then(() => {
+        console.log("Document written");
+        setSuccessMessage('Current User Request Added!')
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+        setErrorMessage(error.message)
+        setLoading(true)
+    });
+    db.collection("friends").doc(userId).collection("relationships").doc(auth.currentUser.uid).set(secondUserStatus)
+    .then(() => {
+        console.log("Document written");
+        setSuccessMessage('Second User Pending Added!')
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+        setErrorMessage(error.message)
+        setLoading(true)
+    });
+  };
 
 const setImage = (image) => {
   // Create a reference to the file we want to download
@@ -58,7 +157,7 @@ const setImage = (image) => {
   .then((url) => {
     // Insert url into an <img> tag to "download"
     setUserProfilePictureURL(url)
-    console.log(userProfilePictureURL)
+    // console.log(userProfilePictureURL)
   })
   .catch((error) => {
     // A full list of error codes is available at
@@ -92,12 +191,35 @@ const setImage = (image) => {
       { userProfilePictureURL &&  <Image source={{uri: userProfilePictureURL,}} style={[AppStyles.profilePic]}/> }
       { !userProfilePictureURL &&  <Icon name='round-account-button-with-user-inside' width='100' height='100' color='gray' /> }     
     </View>
-    <View style={[AppStyles.section]}>
+    <View style={[AppStyles.container]}>
       <Text style={[AppStyles.titleText]}>{userDisplayName} </Text>      
       
     </View>
-    <View style={[AppStyles.section]}>
+    <View style={[AppStyles.container]}>
       <Text style={[AppStyles.titleText]}>{userId}</Text>
+    </View>
+    <View style={[AppStyles.container]}>
+        {relationship === 'none' &&
+        <TouchableOpacity style={{width: '40%', alignItems: 'center'}} onPress={() => friendStatusUpdate('Add')}>
+            <Text style={{ fontSize: 12, borderWidth: 1, borderColor: '#CED0CE', padding: 10 }}>Add Friend</Text>
+        </TouchableOpacity> }
+        {relationship === 'pending' &&
+        <View style={[AppStyles.section]}>
+        <TouchableOpacity style={{width: '40%', alignItems: 'center'}} onPress={() => friendStatusUpdate('Accept')}>
+            <Text style={{ fontSize: 12, borderWidth: 1, borderColor: '#CED0CE', padding: 10 }}>Accept</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{width: '40%', alignItems: 'center'}} onPress={() => friendStatusUpdate('Decline')}>
+            <Text style={{ fontSize: 12, borderWidth: 1, borderColor: '#CED0CE', padding: 10 }}>Decline</Text>
+        </TouchableOpacity> 
+        </View> }
+        {relationship === 'requested' &&
+        <TouchableOpacity style={{width: '40%', alignItems: 'center'}} >
+            <Text style={{ fontSize: 12, borderWidth: 1, borderColor: '#CED0CE', padding: 10 }}>Requested</Text>
+        </TouchableOpacity> }
+        {relationship === 'friends' &&
+        <TouchableOpacity style={{width: '40%', alignItems: 'center'}} onPress={() => friendStatusUpdate('Remove')}>
+            <Text style={{ fontSize: 12, borderWidth: 1, borderColor: '#CED0CE', padding: 10 }}>Remove Friend</Text>
+        </TouchableOpacity> }
     </View>
       </>
 )
